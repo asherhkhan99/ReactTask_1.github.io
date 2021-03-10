5491ad63294fe779f9e4ca53712127dc770d62a4
asherhkhan99 <65026695+asherhkhan99@users.noreply.github.com>
2021-03-10 17:28:34 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './Footer';
+import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Bar from './Bar'
+
+function App(){
+
+    return(
+        <>
+            <Footer/>
+            <Bar/>
+            
+        </>
+    )
+}
+
+export default App;
